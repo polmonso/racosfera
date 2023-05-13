3f7bb5f581618895d025e853cf0eab23a30fc937
quimnuss <lluna.nova@gmail.com>
2023-05-14 01:37:51 +0200

    scatterplot3d, attempts at getting a streamplot working

--- ecohome.py
+++ ecohome.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 
 
@@ -28,7 +29,8 @@ nice_settings = [
               [0, -0.2, -0.04]]
     },
     'http://localhost:8502/?r=-0.3&r=0.08&r=0.8&A=-0.1&A=0.2&A=0.0&A=-0.3&A=-0.1&A=0.1&A=0.0&A=-0.2&A=-0.04&x0=0.6&x0=1.5&x0=10.0',
-    'http://localhost:8502/?r=-0.62&r=0.86&r=-1.0&A=-0.05&A=0.25&A=0.0&A=-0.2&A=-0.1&A=0.0&A=0.0&A=0.0&A=1.0&x0=1.46&x0=2.79&x0=0.0'
+    'http://localhost:8502/?r=-0.62&r=0.86&r=-1.0&A=-0.05&A=0.25&A=0.0&A=-0.2&A=-0.1&A=0.0&A=0.0&A=0.0&A=1.0&x0=1.46&x0=2.79&x0=0.0',
+    'http://localhost:8503/?r=-0.57&r=0.08&r=0.8&A=-0.1&A=0.2&A=0.0&A=-0.3&A=-0.1&A=0.1&A=0.0&A=-0.2&A=-0.04&x0=0.6&x0=1.5&x0=10.0'
 ]
 
 x0 = [0.6, 1.5, 10.0]
@@ -86,11 +88,23 @@ A = st.sidebar.experimental_data_editor(A)
 
 def glv(x, t, r, A):
 
-     x[x < 0.00000001] = 0 # prevent numerical problems
+    x[x < 0.00000001] = 0 # prevent numerical problems
 
-     dxdt = x*(r + A.dot(x))
+    dxdt = x*(r + A.dot(x))
 
-     return dxdt
+    return dxdt
+
+def glv_mesh(mesh_x, r_, A):
+
+    mesh_x[mesh_x < 0.00000001] = 0 # prevent numerical problems
+
+    r_mesh = np.tile(r_, (mesh_x.shape[1], 1))
+
+    mesh_dxdt = mesh_x.T*(r_mesh + A.dot(mesh_x).T)
+
+    mesh_dxdt[mesh_dxdt < 0.00000001] = 0
+
+    return mesh_dxdt
 
 equilibrium = -np.linalg.inv(A).dot(r)
 
@@ -154,16 +168,69 @@ fig = px.scatter(
 )
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
+fig2 = px.scatter_3d(chart_data,
+    x="wolf",
+    y="rabbit",
+    z="grass",
+    hover_name=chart_data.index,
+    color=chart_data.index)
 
-wolves, rabbits, grass = np.meshgrid(np.arange(0,3,0.1), np.arange(0, 3, 0.1), np.arange(0, 3, 0.1))
-mesh = np.array([wolves, rabbits, grass])
-# TODO make glv support mesh as input
-meshdot = glv(mesh, 0, r, A)
-plt.style.use('dark_background')
-fig, ax = plt.subplots()
-ax.streamplot(wolves, rabbits, meshdot[0], meshdot[1])
+st.plotly_chart(fig2, theme="streamlit", use_container_width=True)
 
-st.pyplot(fig)
+
+
+def streamplot():
+
+    # plotly streamlines do not look good either, there's probably something wrong in my reshaping
+
+    wolves, rabbits, grass = np.meshgrid(np.arange(0.1,3.1,0.1), np.arange(0.1, 3.1, 0.1), np.arange(5, 8, 0.1))
+    mesh = np.vstack([wolves.ravel(), rabbits.ravel(), grass.ravel()])
+
+    meshdot = glv_mesh(mesh, r, A)
+
+    # ff streamline can't handle dx/dt = 0
+    meshdot[meshdot < 0.0001] = 0.0001
+
+    u = meshdot.T[0][:].reshape(30,30,30)
+    v = meshdot.T[1][:].reshape(30,30,30)
+    w = meshdot.T[2][:].reshape(30,30,30)
+
+    nslice = st.slider('slice wolves vs rabbits', 0, 30, 14)
+
+    fig = ff.create_streamline(np.arange(0.1,3.1,0.1), np.arange(0.1,3.1,0.1), u[:,:,nslice], v[:,:,nslice], arrow_scale=.1)
+
+    st.plotly_chart(fig)
+
+    fig = ff.create_streamline(np.arange(0.1,3.1,0.1), np.arange(5, 8, 0.1), u[nslice,:,], v[nslice,:,:], arrow_scale=.1)
+    st.plotly_chart(fig)
+
+
+
+    # There's something wrong on how we go from a exploded horizontal array to a streamplot who expects a chessboard overlap
+    # maybe it's because we're going from a 3d streamplot to a 2d slice, but who knows
+
+    # plt.style.use('dark_background')
+    # fig, ax = plt.subplots()
+    # wolves2d = wolves[:,:,0]
+    # rabbits2d = rabbits[:,:,0]
+    # nslice = st.slider('slice wolves vs rabbits', 0, 30, 14)
+    # u = meshdot.T[1][(nslice*900):((nslice+1)*900)].reshape(30,30)
+    # v = meshdot.T[2][(nslice*900):((nslice+1)*900)].reshape(30,30)
+    # ax.streamplot(wolves2d, rabbits2d, u, v)
+
+    # st.pyplot(fig)
+
+    # doesnt work, non-interactive gui it says
+    # u = meshdot.T[1][:].reshape(30,30,30)
+    # v = meshdot.T[2][:].reshape(30,30,30)
+    # w = meshdot.T[2][:].reshape(30,30,30)
+
+    # ax2 = plt.figure().add_subplot(projection='3d')
+    # ax2.quiver(wolves, rabbits, grass, u, v, w, length=0.1, normalize=True)
+
+    # plt.show()
+
+# streamplot()
 
 if ballons:
     st.balloons()

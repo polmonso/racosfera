2e0d08573700d60e7a0fb98391e9f39165e90f5d
quimnuss <lluna.nova@gmail.com>
2023-05-24 00:02:25 +0200

    some more work on query params on multihome

--- multiecohome.py
+++ multiecohome.py
@@ -32,7 +32,7 @@ nice_settings = [
 
 # setup
 
-species_data = [
+default_species_meta = [
     ['🐺',   'wolf', 0.1, -0.3],
     ['🐇', 'rabbit', 0.1,-0.08],
     ['🥕', 'carrot', 0.1, 1.27],
@@ -46,17 +46,18 @@ species_data = [
     ['🌿',  'grass', 0.1,  0.1]
 ]
 
+default_species_meta_columns = ['symbol', 'name', 'initial_density', 'reproduction']
+
+symbols = [species[0] for species in default_species_meta]
+
 st.sidebar.markdown('reproduction is positive for species that survive alone (e.g. plants) and negative otherwise (e.g. carnivores and herbivores).')
 
 st.sidebar.markdown('You can remove a species by selecting its row and pressing Delete')
 
-st.sidebar.markdown('You can **edit the values** by double-clicking. Read the explanation to avoid kill everything :)')
-
-
-default_cross_matrix = {'index':
-    ['symbol',  '🐺', '🐇',  '🥕',  '🦊', '🐦',  '🐝', '🐛', '🐟',  '🌱',  '🍒',  '🌿'],
-    'cross':
-    [
+cross_matrix_columns = [
+     'symbol',  '🐺', '🐇',  '🥕',  '🦊', '🐦',  '🐝', '🐛', '🐟',  '🌱',  '🍒',  '🌿'
+]
+default_cross_matrix = [
         ['🐺',  -0.1,   0.3,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0],
         ['🐇',  -0.4,  -0.1,   0.3,  -0.1,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0],
         ['🥕',   0.0,  -0.3,  -0.2, -0.04,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0],
@@ -68,10 +69,7 @@ default_cross_matrix = {'index':
         ['🌱',   0.0,  -0.2,   0.0,   0.0,  -0.3,  0.15,  -0.4,   0.0,  -0.4,  -0.1, -0.05],
         ['🍒',   0.0,   0.0,   0.0,   0.0,   0.1,   0.0, -0.01,   0.0,   0.0, -0.04,   0.0],
         ['🌿',   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0, -0.04],
-    ]
-}
-
-cross_matrix = default_cross_matrix
+]
 
 if 'initialized' not in st.session_state:
     print('Fetching data from query_params or default')
@@ -84,52 +82,51 @@ if 'initialized' not in st.session_state:
         default_species_meta = np.array(get_query_params['m'])
         default_species_meta = default_species_meta.reshape(default_num_species,4)
         default_species_meta[:,2:3] = default_species_meta[:,2:3].astype(float)
-        species_data = default_species_meta
 
     if 'A' in get_query_params:
-        cross_matrix_cross = [ float(a) for a in get_query_params['A']]
-        symbols = [species[0] for species in species_data]
-        cross_matrix_cross = [[symbol] + cross_matrix_cross[i*len(symbols):(i+1)*len(symbols)] for i,symbol in enumerate(symbols)]
-        symbol_index = ['symbol'] + symbols
-        cross_matrix = {
-            'index': symbol_index,
-            'cross': cross_matrix_cross
-        }
+        qp_cross_matrix = [float(a) for a in get_query_params['A']]
+        symbols = [species[0] for species in default_species_meta]
+        default_cross_matrix = [[symbol] + qp_cross_matrix[i*len(symbols):(i+1)*len(symbols)] for i,symbol in enumerate(symbols)]
 
+species_meta = pd.DataFrame(columns=default_species_meta_columns, data=default_species_meta)
 
 num_species = st.sidebar.slider('Number of species', 1, 10, value=default_num_species)
 
+species_meta = species_meta.iloc[0:num_species]
+species_meta_list = st.sidebar.experimental_data_editor(species_meta, num_rows="dynamic")
 
-all_species_df = pd.DataFrame(columns=['symbol', 'name', 'initial_density', 'reproduction'], data=species_data)
-all_species_meta_df = all_species_df.set_index('symbol')
+# TODO check if a row was removed and do
+# num_species = min(species_meta_list, num_species)
+# default_cross_matrix filter symbols
+# or maybe cross filter symbols
 
-cross_matrix_df = pd.DataFrame(columns=cross_matrix['index'], data=cross_matrix['cross'])
-cross_matrix_df = cross_matrix_df.set_index('symbol')
+cross_col_names = ['symbol'] + [row[0] for row in default_cross_matrix]
+default_cross_matrix = pd.DataFrame(columns=cross_col_names, data=default_cross_matrix)
+default_cross_matrix = default_cross_matrix.set_index('symbol')
+default_cross_matrix = default_cross_matrix.iloc[0:num_species,0:num_species]
+cross = st.sidebar.experimental_data_editor(default_cross_matrix)
 
-# run_cross_matrix_df = cross_matrix_df.iloc[[0,2,3],[0,2,3]]
+# experimental_data_editor only returns lists, we need to rebuild the df
 
-species_meta_df = all_species_meta_df.iloc[0:num_species]
-species_data = cross_matrix_df.iloc[0:num_species,0:num_species]
-species_symbols = cross_matrix['index'][1:num_species]
+# species_meta_df = pd.DataFrame(columns=default_species_meta_columns, data=species_meta_list)
+# species_meta_df = species_meta_df.set_index('symbol')
+
+# cross_df = pd.DataFrame(columns=cross_matrix_columns, data=default_cross_matrix)
+# cross_df = cross_df.set_index('symbol')
 
 
 # TODO getting from params it's gonna be a bit tricky now
 
+# if len(species_meta_df) < num_species:
+#     num_species = len(species_meta_df)
+#     current_species = species_meta_df.index.to_list()
 
-species_meta_df = st.sidebar.experimental_data_editor(species_meta_df, num_rows="dynamic")
+#     previous_species = species_data.index.to_list()
+#     removed_species = [species for species in previous_species if species not in current_species]
+#     species_data = species_data.drop(labels=removed_species, axis=0)
+#     species_data = species_data.drop(labels=removed_species, axis=1)
 
-if len(species_meta_df) < num_species:
-    num_species = len(species_meta_df)
-    current_species = species_meta_df.index.to_list()
-
-    previous_species = species_data.index.to_list()
-    removed_species = [species for species in previous_species if species not in current_species]
-    species_data = species_data.drop(labels=removed_species, axis=0)
-    species_data = species_data.drop(labels=removed_species, axis=1)
-
-cross = st.sidebar.experimental_data_editor(species_data)
-
-experiment_params = {"num": num_species, "m": species_meta_df.reset_index().to_numpy().flatten(), "A": cross.to_numpy().flatten()}
+experiment_params = {"num": len(species_meta_list), "m": species_meta_list.to_numpy().flatten(), "A": cross.to_numpy().flatten()}
 st.experimental_set_query_params(**experiment_params)
 
 
@@ -145,10 +142,9 @@ def glv(x, r, A):
 
 
 A = cross.to_numpy()
-r = species_meta_df['reproduction'].to_numpy()
-x0 = species_meta_df['initial_density'].to_numpy()
-names = species_meta_df.index.to_list()
-
+r = species_meta_list['reproduction'].astype(float).to_numpy()
+x0 = species_meta_list['initial_density'].astype(float)
+names = species_meta_list['symbol']
 equilibrium = -np.linalg.inv(A).dot(r)
 
 st.markdown(f"Given your parameters we have the following results")
@@ -178,7 +174,6 @@ for i in range(numiterations):
     progress_bar.progress((i + 1)/numiterations)
 
     new_row = sim[-1] + glv(sim[-1], r, A)
-
     new_row[new_row < 0.0000001] = 0 # prevent numerical problems
 
     # Update status text.
